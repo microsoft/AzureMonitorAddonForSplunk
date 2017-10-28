2af4c82c32c6258c1e4558d97a31ac387c456e14
Greg Oliver <divingclimber@gmail.com>
2017-10-27 17:27:01 -0700

    add switch for sovereign clouds (#13)

--- bin/azure_activity_log.sh
+++ bin/azure_activity_log.sh
@@ -1,4 +1,12 @@
 #!/bin/bash  
 
+# AZURE_ENVIRONMENT tells the add-on which cloud you're operating in
+# valid values are:
+# AzureCloud = public Azure 
+# AzureUSGovernment = US government clouds, including DoD
+# AzureChinaCloud = China's sovereign cloud
+# AzureGermanCloud = Germany's sovereign cloud
+export AZURE_ENVIRONMENT=AzureCloud
+
 current_dir=$(dirname "$0")
 "$SPLUNK_HOME/bin/splunk" cmd node "$current_dir/app/azure_activity_log.js" $@
