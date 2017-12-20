afc43034d6cce89f73e23024e9790a44b60a5506
nth-block <navneet.ts@gmail.com>
2017-12-20 06:06:21 +0000

    Update am_depends_ubuntu.sh
    Commented the lines above as Splunk has its own Python packages default location and the lines below. Also adding the following lines to have the Node.JS packages and modules installed.

--- packages/am_depends_ubuntu.sh
+++ packages/am_depends_ubuntu.sh
@@ -10,9 +10,23 @@ pip install cryptography -q
 pip install adal -q
 pip install splunk-sdk -q
 pip install futures -q
-cp $PYTHON_SITEPACKAGES/six.py $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp -R $PYTHON_SITEPACKAGES/splunklib $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp -R $PYTHON_SITEPACKAGES/concurrent $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp -R $PYTHON_SITEPACKAGES/adal $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp -R $PYTHON_SITEPACKAGES/jwt $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp -R $PYTHON_SITEPACKAGES/dateutil $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
+#cp $PYTHON_SITEPACKAGES/six.py $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
+#cp -R $PYTHON_SITEPACKAGES/splunklib $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
+#cp -R $PYTHON_SITEPACKAGES/concurrent $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
+#cp -R $PYTHON_SITEPACKAGES/adal $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
+#cp -R $PYTHON_SITEPACKAGES/jwt $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
+#cp -R $PYTHON_SITEPACKAGES/dateutil $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
+
+#Commented the lines above as Splunk has its own Python packages default location and the lines below
+cp $PYTHON_SITEPACKAGES/six.py $SPLUNK_HOME/lib/python2.7/site-packages
+cp -R $PYTHON_SITEPACKAGES/splunklib $SPLUNK_HOME/lib/python2.7/site-packages
+cp -R $PYTHON_SITEPACKAGES/concurrent $SPLUNK_HOME/lib/python2.7/site-packages
+cp -R $PYTHON_SITEPACKAGES/adal $SPLUNK_HOME/lib/python2.7/site-packages
+cp -R $PYTHON_SITEPACKAGES/jwt $SPLUNK_HOME/lib/python2.7/site-packages
+cp -R $PYTHON_SITEPACKAGES/dateutil $SPLUNK_HOME/lib/python2.7/site-packages
+
+#Adding the following lines to have the Node.JS packages and modules installed
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install -y nodejs
+cd $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin/app
+npm install
