d7ea8131001daa06106b5708663f4572cc931776
Rick Rainey <rickra@microsoft.com>
2018-04-18 15:59:04 -0500

    Changed packaging script to use the system's temp folder instead of creating a new temp folder within the add-on repo.

--- deployment/package.cmd
+++ deployment/package.cmd
@@ -1,21 +1,21 @@
 set version=TA-Azure_Monitor_1_2_6
 cd deployment
-del temp\%version%.spl
+del %temp%\%version%.spl
 del ..\packages\%version%.spl
-7z a -ttar temp\%version%.tar TA-Azure_Monitor\bin\*.cmd
-7z a -ttar temp\%version%.tar TA-Azure_Monitor\bin\*.sh
-7z a -ttar temp\%version%.tar TA-Azure_Monitor\bin\*.py
-7z a -ttar temp\%version%.tar TA-Azure_Monitor\bin\sourcetypes.json
-7z a -ttar temp\%version%.tar TA-Azure_Monitor\bin\app\*.js
-7z a -ttar temp\%version%.tar TA-Azure_Monitor\bin\app\*.json
-7z a -ttar temp\%version%.tar TA-Azure_Monitor\bin\app\*.py
-7z a -ttar temp\%version%.tar TA-Azure_Monitor\static\*.*
-7z a -ttar temp\%version%.tar TA-Azure_Monitor\default\*
-7z a -ttar temp\%version%.tar TA-Azure_Monitor\README\*
-7z a -ttar temp\%version%.tar TA-Azure_Monitor\LICENSE
-7z a -ttar temp\%version%.tar TA-Azure_Monitor\README.md
-copy temp\%version%.tar temp\%version%.spl
-7z a -tgzip ..\packages\%version%.spl temp\%version%.spl
-del temp\%version%.spl
-del temp\%version%.tar
+7z a -ttar %temp%\%version%.tar TA-Azure_Monitor\bin\*.cmd
+7z a -ttar %temp%\%version%.tar TA-Azure_Monitor\bin\*.sh
+7z a -ttar %temp%\%version%.tar TA-Azure_Monitor\bin\*.py
+7z a -ttar %temp%\%version%.tar TA-Azure_Monitor\bin\sourcetypes.json
+7z a -ttar %temp%\%version%.tar TA-Azure_Monitor\bin\app\*.js
+7z a -ttar %temp%\%version%.tar TA-Azure_Monitor\bin\app\*.json
+7z a -ttar %temp%\%version%.tar TA-Azure_Monitor\bin\app\*.py
+7z a -ttar %temp%\%version%.tar TA-Azure_Monitor\static\*.*
+7z a -ttar %temp%\%version%.tar TA-Azure_Monitor\default\*
+7z a -ttar %temp%\%version%.tar TA-Azure_Monitor\README\*
+7z a -ttar %temp%\%version%.tar TA-Azure_Monitor\LICENSE
+7z a -ttar %temp%\%version%.tar TA-Azure_Monitor\README.md
+copy %temp%\%version%.tar %temp%\%version%.spl
+7z a -tgzip ..\packages\%version%.spl %temp%\%version%.spl
+del %temp%\%version%.spl
+del %temp%\%version%.tar
 exit
