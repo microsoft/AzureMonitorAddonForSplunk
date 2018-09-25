428ccbef6e0cf6146b54290ec92a4bc2b930b289
Greg Oliver <divingclimber@gmail.com>
2018-09-25 11:25:41 +0100

    V131 (#78)
    * improve messages when firewall prevents connection
    
    * refactor am_depends for ubuntu and rhel, remove linux_dependencies.sh
    
    * update package for 1.3.1

--- packages/am_depends_rhel.sh
+++ packages/am_depends_rhel.sh
@@ -2,15 +2,6 @@ export SPLUNK_HOME=/opt/splunk
 export PYTHON_SITEPACKAGES=/usr/lib/python2.7/site-packages
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
-pip install six -I 
-pip install adal 
-pip install splunk-sdk 
-pip install futures
 pip install msrestazure -t $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp $PYTHON_SITEPACKAGES/six.py $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp -R $PYTHON_SITEPACKAGES/splunklib $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp -R $PYTHON_SITEPACKAGES/splunklib $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin/app
-cp -R $PYTHON_SITEPACKAGES/concurrent $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp -R $PYTHON_SITEPACKAGES/adal $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp -R $PYTHON_SITEPACKAGES/jwt $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp -R $PYTHON_SITEPACKAGES/dateutil $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
+pip install splunk-sdk  -t $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
+pip install futures -t $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
--- packages/am_depends_ubuntu.sh
+++ packages/am_depends_ubuntu.sh
@@ -3,18 +3,7 @@ export PYTHON_SITEPACKAGES=/usr/local/lib/python2.7/dist-packages
 apt-get update -q -y
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
-apt-get -q -y install build-essential libssl-dev libffi-dev python-dev
-pip install Markdown -q
-pip install six -I -q 
-pip install PyJWT -q
-pip install cryptography -q
-pip install adal -q
-pip install splunk-sdk -q
-pip install futures -q
 pip install msrestazure -t $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp $PYTHON_SITEPACKAGES/six.py $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp -R $PYTHON_SITEPACKAGES/splunklib $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp -R $PYTHON_SITEPACKAGES/concurrent $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp -R $PYTHON_SITEPACKAGES/adal $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp -R $PYTHON_SITEPACKAGES/jwt $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
-cp -R $PYTHON_SITEPACKAGES/dateutil $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
+pip install Markdown -q -t $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
+pip install splunk-sdk -q -t $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
+pip install futures -q -t $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
