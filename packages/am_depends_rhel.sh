2dd63e9d833b5589e7ad0af9f289beea84d27163
Greg Oliver <golive@microsoft.com>
2018-10-16 11:17:01 +0100

    install python splunklib into bin/app

--- packages/am_depends_rhel.sh
+++ packages/am_depends_rhel.sh
@@ -4,4 +4,5 @@ wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 pip install msrestazure -t $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
 pip install splunk-sdk  -t $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
+pip install splunk-sdk  -t $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin/app
 pip install futures -t $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
--- packages/am_depends_ubuntu.sh
+++ packages/am_depends_ubuntu.sh
@@ -6,4 +6,5 @@ python get-pip.py
 pip install msrestazure -t $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
 pip install Markdown -q -t $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
 pip install splunk-sdk -q -t $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
+pip install splunk-sdk -q -t $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin/app
 pip install futures -q -t $SPLUNK_HOME/etc/apps/TA-Azure_Monitor/bin
